eebbc3523d7eca859613c7b5fd24ac35a4707703
Nikhil Bhagat <nikhilbhagat424@gmail.com>
2024-06-03 22:47:54 +0530

    fix build errors

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -39,7 +39,7 @@ export async function GET() {
             
       
             const updatedProduct = await Product.findOneAndUpdate(
-              { url: product.url },
+              { url: scrapedProduct.url },
               product,
             );
 
